f748d690729149c8d34394f068f316b99ab6e9da
hopeshunta <hope.shunta@gmail.com>
2024-11-20 19:22:48 -0600

    end of practice

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ObjectClasses/RobotSubsystems/RobotTypeParams/DriveParams.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ObjectClasses/RobotSubsystems/RobotTypeParams/DriveParams.java
@@ -17,34 +17,32 @@ public class DriveParams {
 
         // drive model parameters
         MecanumDrive.PARAMS.inPerTick = 1;
-        MecanumDrive.PARAMS.lateralInPerTick = 0.7145464020405548;
-        MecanumDrive.PARAMS.trackWidthTicks = 11.5;
+        MecanumDrive.PARAMS.lateralInPerTick = 0.6591503480757255;
+        MecanumDrive.PARAMS.trackWidthTicks = 11.6;
 
         // feedforward parameters (in tick units)
-        MecanumDrive.PARAMS.kS = 1.1949887108295525;
-        MecanumDrive.PARAMS.kV = 0.1256588154090211;
+        MecanumDrive.PARAMS.kS = 1.3124635507784173;
+        MecanumDrive.PARAMS.kV = 0.12755896485170928;
         MecanumDrive.PARAMS.kA = 0.03;
 
         // path profile parameters (in inches)
-        MecanumDrive.PARAMS.maxWheelVel = 32;
-        MecanumDrive.PARAMS.minProfileAccel = -32;
-        MecanumDrive.PARAMS.maxProfileAccel = 32;
+        MecanumDrive.PARAMS.maxWheelVel = 28;
+        MecanumDrive.PARAMS.minProfileAccel = -28;
+        MecanumDrive.PARAMS.maxProfileAccel = 28;
 
         // turn profile parameters (in radians)
-        MecanumDrive.PARAMS.maxAngVel = Math.PI; // shared with path
-        MecanumDrive.PARAMS.maxAngAccel = Math.PI;
+        MecanumDrive.PARAMS.maxAngVel = Math.PI*2.5; // shared with path
+        MecanumDrive.PARAMS.maxAngAccel = Math.PI*2.5;
 
         // path controller gains
-        MecanumDrive.PARAMS.axialGain = 8;
-        MecanumDrive.PARAMS.lateralGain = 1;
-        MecanumDrive.PARAMS.headingGain = 6; // shared with turn
+        MecanumDrive.PARAMS.axialGain = 12;
+        MecanumDrive.PARAMS.lateralGain = 12;
+        MecanumDrive.PARAMS.headingGain = 12; // shared with turn
 
-        MecanumDrive.PARAMS.axialVelGain = .1;
+        MecanumDrive.PARAMS.axialVelGain = 0;
         MecanumDrive.PARAMS.lateralVelGain = 0;
         MecanumDrive.PARAMS.headingVelGain = 0; // shared with turn
 
-        PinpointDrive.PARAMS.xOffset = -5;
-        PinpointDrive.PARAMS.yOffset = 3;
         PinpointDrive.PARAMS.encoderResolution = GoBildaPinpointDriverRR.goBILDA_4_BAR_POD;
         PinpointDrive.PARAMS.xDirection = GoBildaPinpointDriver.EncoderDirection.FORWARD;
         PinpointDrive.PARAMS.yDirection = GoBildaPinpointDriver.EncoderDirection.REVERSED;
